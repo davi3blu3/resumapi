62cb88b887b4c04501a16cce6de33eed75ff2ac3
Dave Harned <dharned@gmail.com>
2017-02-01 22:09:37 -0600

    retrieve resume from mongo

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 var MongoClient = require('mongodb').MongoClient,
     assert = require('assert');
 
-MongoClient.connect('mongodb://localhost:27017/resumes', function(err, db){
+MongoClient.connect('mongodb://localhost:27017/resumapi', function(err, db){
 
     assert.equal(err, null);
     console.log('Successfully connected to databse');
 
+    db.collection('resumes').find().toArray(function(err, docs){
+
+        assert.equal(err, null);
+        assert.notEqual(docs.length, 0);
+
+        docs.forEach(function(doc){
+            console.log(doc);
+        })
+
+    });
+
+    db.close();
 });
